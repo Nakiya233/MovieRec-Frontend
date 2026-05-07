307231a29762a62dbadeeaad0253034d50370bf2
Nakiya233 <nakiya0217@gmail.com>
2026-05-07 17:39:27 +0800

    feat: 串联根组件（AppHeader + router-view + AppFooter）

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <script setup lang="ts">
+import { computed, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { useAuthStore } from '@/stores/authStore'
+import AppHeader from '@/components/common/AppHeader.vue'
+import AppFooter from '@/components/common/AppFooter.vue'
+
+const route = useRoute()
+const auth = useAuthStore()
+
+const isAdminRoute = computed(() => route.path.startsWith('/admin'))
+
+onMounted(() => {
+  auth.initFromStorage()
+})
 </script>
 
 <template>
+  <AppHeader v-if="!isAdminRoute" />
   <router-view />
+  <AppFooter v-if="!isAdminRoute" />
 </template>
